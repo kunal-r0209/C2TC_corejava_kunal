4321d7baf01909938f6c14cb4cf79dc913b3506e
HP <HP@LAPTOP-RDID7QB7>
2025-07-21 23:34:24 +0530

    CharDemo

---
+++ C2TC_corejava/src/day1/IdentifiersDemo.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class IdentifiersDemo {
+	public static void main(String[] args) {
+		
+		int $number = 20; 
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Ramya"; 
+		System.out.println("value is : "+ studentName);
+		
+	
+	}
+
+}
+
+
