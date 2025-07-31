7335a4f1ec9ac388e214afac69f1138a62625071
HP <HP@LAPTOP-RDID7QB7>
2025-07-31 11:10:47 +0530

    day11

---
+++ C2TC_corejava_/src/Day8/interfaces/extendinginterfaces/ChildInterface.java
@@ -0,0 +1,8 @@
+package Day8.interfaces.extendinginterfaces;
+
+
+	public interface ChildInterface extends InterfaceOne {
+		void show();
+	}
+
+
---
+++ C2TC_corejava_/src/Day8/interfaces/functionalinterfaces/GreetInterface.java
@@ -0,0 +1,6 @@
+package Day8.interfaces.functionalinterfaces;
+
+@FunctionalInterface
+public interface GreetInterface {
+	public String greet();
+}
